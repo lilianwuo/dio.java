343c266b1023d99b85f887976f8a341878b3c81a
Lilian Wuo <wuolilian@gmail.com>
2025-06-18 15:43:43 -0300

    aula 3

---
+++ meu-primeiro-projeto-java/src/com/auladio/tiposvariaveis/Ternario.java
@@ -0,0 +1,16 @@
+package com.auladio.tiposvariaveis;
+
+public class Ternario {
+
+    public static void main(String[] args) {
+        
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+        
+        System.out.println(resultado);
+    }
+}
