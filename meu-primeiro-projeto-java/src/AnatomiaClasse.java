7a038f74b0068d9b1c9791c58adcbec27f9200f9
Lilian Wuo <wuolilian@gmail.com>
2025-06-18 08:55:27 -0300

    atualização aula 1

--- meu-primeiro-projeto-java/src/AnatomiaClasse.java
+++ meu-primeiro-projeto-java/src/AnatomiaClasse.java
@@ -1,22 +1,16 @@
 public class AnatomiaClasse {
 
-    public static void main (String [] args) {
+    public static void main (String[] args) {
         
         String primeiroNome = "Lilian";
         String segundoNome = "Wuo";
-
         String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
         System.out.println(nomeCompleto);
-    }
 
-        public static String nomeCompleto (String primeiroNome, String segundoNome);{
-            return primeiroNome + " " + segundoNome;
+        }
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome + " " + segundoNome;
    
         }   
 
     }
-
-
-
-
-
